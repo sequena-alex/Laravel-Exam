0316d8636c134ef7fce84f2cdf301a5fa356f2fe
Alex Sequena <sequena.alex@gmail.com>
2021-06-22 18:59:06 +0800

    Datatables INIT

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('company_name')->nullable();
+            $table->string('company_logo')->nullable();
+            $table->string('company_website')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
